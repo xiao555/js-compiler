10c8e3bbc3b25c9bf913f85a96c5139ac55d560f
xiao555 <zhangruiwu32@gmail.com>
2016-12-13 03:53:22 +0800

    update

--- assets/main.js
+++ assets/main.js
@@ -68,7 +68,9 @@
 	(0, _jquery2.default)(document).ready(function ($) {
 	  // var operator = ['+','-','*','/','**'];
 	  // console.log(!!~operator.indexOf('+')); // true
-	
+	  $(window).resize(function () {
+	    $('canvas').attr("width", $('#canvas').width());
+	  });
 	  // Init CodeMirror
 	  var editor = _codemirror2.default.fromTextArea(document.getElementById("codearea"), {
 	    lineNumbers: true,
@@ -151,18 +153,21 @@
 	'use strict';
 	
 	// Global variables
+	// Recording Current Line
 	var line = 1;
-	
+	// Reserved symbol table
 	var TokenTable = [['CONST_ID', "PI", 3], ['CONST_ID', "E", 2], ['T', "T", 0.0], ['FUNC', "SIN", 0.0], ['FUNC', "COS", 0.0], ['FUNC', "TAN", 0.0], ['FUNC', "LOG", 0.0], ['FUNC', "EXP", 0.0], ['FUNC', "SQRT", 0.0], ['ORIGIN', "ORIGIN", 0.0], ['SCALE', "SCALE", 0.0], ['ROT', "ROT", 0.0], ['IS', "IS", 0.0], ['FOR', "FOR", 0.0], ['FROM', "FROM", 0.0], ['TO', "TO", 0.0], ['STEP', "STEP", 0.0], ['DRAW', "DRAW", 0.0]];
-	
+	// Recording data
 	var origin_x, origin_y, rot, scale_x, scale_y;
 	
-	// 词法分析
-	// output = [
-	// {type: "",value: "",...},
-	// {type: "",value: "",...},
-	// ...
-	// ]
+	/**
+	* 词法分析
+	* output = [
+	*   {type: "",value: "",...},
+	*   {type: "",value: "",...},
+	*   ...
+	* ]
+	*/
 	function lexer(input) {
 	  var current = 0;
 	  var tokens = [];
@@ -272,10 +277,26 @@
 	  return tokens;
 	}
 	
-	// AST 抽象语法树
-	// output:
-	// {type: 'Program', body: [...] }
-	// 
+	/** AST 抽象语法树
+	* output: = [
+	*   {type: 'Program', body: [
+	*     {type: 'statement', name: "ROT", value: 0},
+	*     {type: 'statement', name: "SCALE", value: [
+	*       0: "20",
+	*       1: "20"
+	*     ]},
+	*     {type: 'statement', name: "FOR", params: [
+	*       0: "T",
+	*       1: "0",
+	*       2: {type: "expr", name: "*", lvalue: "2", rvalue: "3.1415926"},
+	*       3: "0.1",
+	*       4: {type: "Expression", name: "COS", params: "T"},
+	*       5: {type: "Expression", name: "SIN", params: "T"},
+	*     ]}
+	*   ]}
+	* ]
+	* 
+	*/
 	function parser(tokens) {
 	  console.log("语法分析:");
 	  line = 1;
@@ -286,12 +307,13 @@
 	  };
 	  while (current < tokens.length) {
 	    ast.body.push(walk());
-	    console.log(ast);
+	    // console.log(ast);
 	  }
 	
+	  // 计算表达式
 	  function factor() {
 	    var token = tokens[current];
-	    console.log(token.value);
+	    // console.log(token.value);
 	    // paren
 	    if (token.type === 'PAREN' && token.value === '(') {
 	      current++;
@@ -310,7 +332,7 @@
 	    } else if (token.type === 'FUNC') {
 	      current++;
 	      var node = {
-	        type: 'CallExpression',
+	        type: 'Expression',
 	        name: token.value,
 	        params: []
 	      };
@@ -318,13 +340,13 @@
 	      return node;
 	    } else {
 	      current++;
-	      console.log("factor end: " + tokens[current].value);
+	      // console.log("factor end: "+ tokens[current].value);
 	      return token.value;
 	    }
 	  }
 	
 	  function term_tail(lvalue) {
-	    console.log('term_tail start: ' + lvalue + tokens[current].value);
+	    // console.log('term_tail start: ' + lvalue + tokens[current].value);
 	    var token = tokens[current];
 	    if (token.value === '*' || token.value === '/') {
 	      current++;
@@ -337,7 +359,7 @@
 	      node.rvalue = expr();
 	      return node;
 	    } else {
-	      console.log('term_tail end: ' + tokens[current].value);
+	      // console.log('term_tail end: '+ tokens[current].value);
 	      return lvalue;
 	    }
 	  }
@@ -348,7 +370,7 @@
 	  }
 	
 	  function expr_tail(lvalue) {
-	    console.log('expr_tail start: ' + lvalue + tokens[current].value);
+	    // console.log('expr_tail start: '+lvalue+  tokens[current].value);
 	    var token = tokens[current];
 	    if (token.value === '+' || token.value === '-') {
 	      current++;
@@ -370,29 +392,30 @@
 	      node.rvalue = expr();
 	      return node;
 	    } else {
-	      console.log('expr_tail end: ' + lvalue + tokens[current].value);
+	      // console.log('expr_tail end: '+lvalue+ tokens[current].value);
 	      return lvalue;
 	    }
 	  }
-	
+	  // 计算表达式入口
 	  function expr() {
-	    console.log("expr: " + tokens[current].value);
+	    // console.log("expr: " + tokens[current].value);
 	    var lvalue = term();
 	    return expr_tail(lvalue);
 	  }
 	
+	  // 语法分析入口
 	  function walk() {
 	    var token = tokens[current];
-	    console.log('walk', token.value);
+	    // console.log('walk',token.value);
 	    // IS
 	    if (token.type === 'IS') {
 	      current++;
-	      console.log('IS:' + tokens[current].value);
+	      // console.log('IS:' + tokens[current].value);
 	      return expr();
 	    }
 	    // SEMIC
 	    if (token.type === 'SEMIC') {
-	      console.log(";");
+	      // console.log(";");
 	      current++;
 	      if (current >= tokens.length) {
 	        return;
@@ -402,7 +425,7 @@
 	    }
 	    // )
 	    if (token.value === ')') {
-	      console.log(")");
+	      // console.log(")");
 	      current++;
 	      if (current >= tokens.length) {
 	        return;
@@ -411,7 +434,7 @@
 	    }
 	
 	    if (token.type === 'ORIGIN' || token.type === 'SCALE' || token.type === 'ROT') {
-	      console.log(token.type);
+	      // console.log(token.type);
 	      current++;
 	      var node = {
 	        type: 'statement',
@@ -419,7 +442,7 @@
 	        value: []
 	      };
 	      node.value = walk();
-	      console.log('保留字：' + node.value + ' ' + tokens[current].value);
+	      // console.log('保留字：'+ node.value + ' ' + tokens[current].value);
 	      if (tokens[current].value === ',') {
 	        current++;
 	        var _value = node.value;
@@ -431,7 +454,7 @@
 	    }
 	
 	    if (token.type === 'FOR') {
-	      console.log(token.type);
+	      // console.log(token.type);
 	      current++;
 	      var node = {
 	        type: 'statement',
@@ -456,7 +479,7 @@
 	              current++;
 	              node.params.push(expr());
 	              token = tokens[current];
-	              console.log("DRAW:" + token.value);
+	              // console.log("DRAW:"+token.value );
 	              while (token.value != ',') {
 	                current++;
 	                token = tokens[current];
@@ -477,6 +500,7 @@
 	  return ast;
 	}
 	
+	// 从语法树生成代码
 	function codeGenerator(node) {
 	  if (typeof node == 'string' || typeof node == 'number') {
 	    return node;
@@ -485,11 +509,42 @@
 	
 	    case 'expr':
 	      return "(" + codeGenerator(node.lvalue) + node.name + codeGenerator(node.rvalue) + ")";
-	    case 'CallExpression':
+	    case 'Expression':
 	      return '(Math.' + node.name.toLowerCase() + "(" + node.params + "))";
 	    default:
-	      throw new TypeError(node);
+	      throw new TypeError(node.type);
 	  }
+	}
+	
+	// 加法函数，浮点数相加更精确
+	function accAdd(arg1, arg2) {
+	  var r1, r2, m, c;
+	  try {
+	    r1 = arg1.toString().split(".")[1].length;
+	  } catch (e) {
+	    r1 = 0;
+	  }
+	  try {
+	    r2 = arg2.toString().split(".")[1].length;
+	  } catch (e) {
+	    r2 = 0;
+	  }
+	  c = Math.abs(r1 - r2);
+	  m = Math.pow(10, Math.max(r1, r2));
+	  if (c > 0) {
+	    var cm = Math.pow(10, c);
+	    if (r1 > r2) {
+	      arg1 = Number(arg1.toString().replace(".", ""));
+	      arg2 = Number(arg2.toString().replace(".", "")) * cm;
+	    } else {
+	      arg1 = Number(arg1.toString().replace(".", "")) * cm;
+	      arg2 = Number(arg2.toString().replace(".", ""));
+	    }
+	  } else {
+	    arg1 = Number(arg1.toString().replace(".", ""));
+	    arg2 = Number(arg2.toString().replace(".", ""));
+	  }
+	  return (arg1 + arg2) / m;
 	}
 	
 	function droploop(params) {
@@ -497,46 +552,32 @@
 	  var ctx = canvas.getContext("2d");
 	
 	  ctx.fillStyle = "rgb(200,0,0)";
-	  ctx.fillRect(0, 0, 10, 10);
-	  // function getString(object) {
-	  //   console.log("hhhhhhhhhhhhhhhhhhhhhhhhhhhh");
-	  //   for (var i = object.length - 1; i >= 0; i--) {
-	  //     if(typeof(object[i]) == 'object') {
-	  //       console.log("Object");
-	  //       var str = getString(object[i]);
-	  //       object[i] = "";
-	  //       object[i] += str;
-	  //       console.log(str);
-	  //     }
-	  //   }
-	  //   return object.type == 'expr' ? "("+object.lvalue+object.name+object.rvalue+")" : "(Math."+object.name.toLowerCase()+"("+object.params+"))";
-	  // }
+	  ctx.fillRect(0, 0, 1, 1);
 	
 	  for (var i = params.length - 1; i >= 0; i--) {
 	    params[i] = codeGenerator(params[i]);
-	    console.log(params[i]);
+	    // console.log(params[i]);
 	  }
 	  var _from = eval(params[1]);
 	  var _to = eval(params[2]);
 	  var _step = eval(params[3]);
 	  var _x = params[4];
 	  var _y = params[5];
-	  console.log(_from, _to, _step, _x, _y);
+	  // console.log(_from,_to,_step,_x,_y);
 	  // }
 	  // 绘制像素
 	  var myImageData = ctx.getImageData(0, 0, 1, 1);
+	  console.log("    FOR: T", _from, _to, _x, _y);
 	  for (var T = _from; T <= _to; T += _step) {
+	    // 获取原始的x,y
 	    var x = eval(_x);
 	    var y = eval(_y);
-	    console.log(x, y);
-	    x = parseInt(origin_x) + parseInt(x * scale_x * Math.cos(rot)) + parseInt(y * scale_y * Math.sin(rot));
-	    y = parseInt(origin_y) + parseInt(y * scale_y * Math.cos(rot)) + parseInt(x * scale_x * Math.sin(rot));
-	    console.log(x, y);
-	
-	    // ctx.putImageData(myImageData,T,50);
-	    // ctx.putImageData(myImageData,origin_x+parseInt((eval(_x)*scale_x)*Math.cos(rot)+(eval(_y)*scale_y)*Math.sin(rot)), origin_y+parseInt((eval(_y)*scale_y)*Math.cos(rot)+(eval(_x)*scale_x)*Math.sin(rot)));
-	    // ctx.putImageData(myImageData,origin_x+parseInt((eval(_x)*scale_x)), origin_y+parseInt((eval(_y)*scale_y)));
-	    ctx.putImageData(myImageData, x, y);
+	    // 获取进行一系列变化后的x,y
+	    var dx = accAdd(accAdd(origin_x, x * scale_x * Math.cos(rot)), y * scale_y * Math.sin(rot));
+	    var dy = accAdd(accAdd(origin_y, y * scale_y * Math.cos(rot)), -x * scale_x * Math.sin(rot));
+	    // console.log(dx,dy);
+	    // 绘制坐标
+	    ctx.putImageData(myImageData, dx, dy);
 	  }
 	}
 	
@@ -547,15 +588,18 @@
 	    console.log("  Statement " + i);
 	    switch (ast.body[i].name) {
 	      case "ROT":
-	        rot = ast.body[i].value;
+	        rot = eval(codeGenerator(ast.body[i].value));
+	        console.log("    ROT:" + rot);
 	        break;
 	      case "ORIGIN":
 	        origin_x = ast.body[i].value[0];
 	        origin_y = ast.body[i].value[1];
+	        console.log("    ORIGIN:" + origin_x, origin_y);
 	        break;
 	      case "SCALE":
 	        scale_x = ast.body[i].value[0];
 	        scale_y = ast.body[i].value[1];
+	        console.log("    SCALE:" + scale_x, scale_y);
 	        break;
 	      case "FOR":
 	        droploop(ast.body[i].params);
